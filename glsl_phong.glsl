ffc84ca7fce515a84e7dc08d89cefd553fd9df32
Ilya <mokill44@gmail.com>
2022-07-29 08:57:17 +0300

    Update glsl_phong.glsl

--- glsl_phong.glsl
+++ glsl_phong.glsl
@@ -2,12 +2,12 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = fragCoord/iResolution.xy;
-    vec3 light_source = vec3(15.0, 0.0  +  10.0 * sin(iTime), 10.0);
+    vec3 light_source = vec3(10.0, 0.0  +  10.0 * sin(iTime), 10.0);
     float distance_to_plate = 10.0;
     vec2 plateReso = vec2 (32.0, 18.0);
     vec3 luch_zren = vec3(distance_to_plate , (uv.x - 0.5)*plateReso.x ,
         (uv.y - 0.5) * plateReso.y);
-    vec3 sphere_center = vec3 (20.0 + 5.0 * sin(iTime),
+    vec3 sphere_center = vec3 (12.0 ,
     .0+ 2.0*sin(iTime),.0+2.0*cos(iTime));
     float s_R = 5.0;
     float a = dot(luch_zren, luch_zren);
@@ -19,13 +19,20 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         fragColor = vec4(.2,.0,.0,1.0);
         float t0 = (-b-sqrt(D))/(2.0*a);
         vec3 onSphere = t0*luch_zren;
-        vec3 p = onSphere - sphere_center;
+        vec3 n = onSphere - sphere_center;
         vec3 q = light_source - sphere_center;
-        p = normalize(p);
+        n = normalize(n);
         q = normalize(q);
-        float diff = dot(p,q);
+        float diff = dot(n,q);
         if (diff > 0.0) {
             fragColor += diff * vec4(.5,.0,.0,.0);
+        }
+        vec3 v = normalize(onSphere);
+        vec3 r = reflect(v,n);
+  
+        if (dot(r,q) > 0.0) {
+            float spec = pow (dot(r,q),10.0);
+            fragColor += spec * vec4(.3,.3,.3,.0);
         }
         
         
